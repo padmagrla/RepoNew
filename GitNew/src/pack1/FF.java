29e28d742895f82f15f6cc46f2b7a497d58893dc
paddu <padmagrla@gmail.com>
2017-10-30 10:24:25 +0100

    First Commit in GitNew

---
+++ GitNew/src/pack1/FF.java
@@ -0,0 +1,23 @@
+package pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FF {
+	@Test
+	public void testFF()
+	{
+
+			WebDriver driver=new FirefoxDriver();
+			driver.get("http://www.facebook.com");
+			 
+	        System.out.println("Application title is ============="+driver.getTitle());
+	        Assert.assertTrue(false);
+
+	        driver.quit();
+
+		}
+
+}
